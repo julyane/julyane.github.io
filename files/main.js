4866d823e011a22418aae53091872f305f07152c
Fábio Rocha <fabiorochafg@gmail.com>
2015-07-13 10:16:17 -0300

    Adicionando google analyctis

--- files/main.js
+++ files/main.js
@@ -63,5 +63,5 @@ $(window).load(function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-41913842-1', 'fabiorochafg.github.io');
+ga('create', 'UA-41737928-15', 'auto');
 ga('send', 'pageview');
